9834342f1ae9085ec8d031e95e8af5bd4800540a
matsui-37 <matsuigyouhiroshi@matsuigyouhiroshinoMacBook-puro.local>
2018-10-31 19:57:55 +0900

    First

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+
+Bundler.require(*Rails.groups)
+
+module SubMercari
+  class Application < Rails::Application
+
+    config.load_defaults 5.1
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+
+  end
+end
